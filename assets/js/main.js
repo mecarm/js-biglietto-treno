014651c12d7b5e9c1d916fce1a80ae09927db9e8
mecarm <melecarmine99@gmail.com>
2022-10-19 15:15:57 +0200

    Creata funzione calcolo prezzo e sconto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,29 @@
 
 function valida(){
+    let price = 0.21;
     let valueKm = document.getElementById('km').value;
     let valueAge = document.getElementById('eta').value;
         console.log(valueKm);
         console.log(valueAge);
 
-    document.getElementById('preventivo').innerHTML = 'ok'
+    let kmPrice = valueKm * price;
+        console.log(kmPrice);
+
+    let discount;
+    if (valueAge < 18) {
+        discount = 20;
+        document.getElementById('preventivo').innerHTML = kmPrice - ((kmPrice * discount)  / 100);
+    } else if (valueAge > 64){
+        discount = 40;
+        document.getElementById('preventivo').innerHTML = kmPrice - ((kmPrice * discount)  / 100);
+    } else {
+        discount = 0
+        document.getElementById('preventivo').innerHTML = kmPrice;
+    }
+    console.log(discount);
+    
+
+    
     
 
 }
