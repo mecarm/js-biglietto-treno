43283e9178f8bffc22d06cf2765d7534c300a7b4
mecarm <melecarmine99@gmail.com>
2022-10-19 14:34:37 +0200

    Creo input dell'utente

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+function valida(){
+    let valueKm = document.getElementById('km').value;
+    let valueAge = document.getElementById('eta').value;
+        console.log(valueKm);
+        console.log(valueAge);
+
+    document.getElementById('preventivo').innerHTML = 'ok'
+    
+
+}
